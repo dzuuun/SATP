b1d1ec4fc9954f9c0fb118cb99ed23eb217d0873
Yrrej Dzun Deypalubos <yrrejdzun@gmail.com>
2023-08-09 15:34:11 +0800

    implemented sidebar

--- client/transaction/script.js
+++ client/transaction/script.js
@@ -141,3 +141,29 @@ function setErrorMessage(message) {
     $(".alert").alert("close");
   }, 2000);
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.querySelector("footer").style.marginLeft = "250px";
+  nav = true;
+}
+
+var nav = false;
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.querySelector("footer").style.marginLeft = "0";
+  nav = false;
+}
+function toggleNav() {
+  nav ? closeNav() : openNav();
+}
+
+let signOutButton = document.getElementById("signout");
+
+signOutButton.addEventListener("click", () => {
+  sessionStorage.clear();
+  window.location.href = "../../index.html";
+});
