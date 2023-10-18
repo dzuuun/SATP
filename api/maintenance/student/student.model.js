32173ba9fcfc0bcaa0c5518d28b5042f37b8097e
Yrrej Dzun Deypalubos <yrrejdzun@gmail.com>
2023-10-18 14:10:11 +0800

    api fix

--- api/maintenance/student/student.model.js
+++ api/maintenance/student/student.model.js
@@ -3,7 +3,7 @@ const pool = require("../../../db/db");
 module.exports = {
   getAllStudent: (callBack) => {
     pool.query(
-      "SELECT users.id, users.username, CONCAT( user_info.givenname, ' ', user_info.surname ) AS name, courses.code AS course, users.is_active FROM users INNER JOIN user_info ON users.id = user_info.user_id INNER JOIN courses ON user_info.course_id=courses.id WHERE users.is_student_rater = 1",
+      "SELECT users.id, users.username, CONCAT( user_info.surname, ', ', user_info.givenname ) AS name, courses.code AS course, users.is_active FROM users INNER JOIN user_info ON users.id = user_info.user_id INNER JOIN courses ON user_info.course_id=courses.id WHERE users.is_student_rater = 1",
       (error, results) => {
         if (error) {
           callBack(error);
@@ -57,6 +57,7 @@ module.exports = {
               data.is_active,
             ],
             (error, results) => {
+              console.log(error)
               if (error) {
                 callBack(error);
               }
