1e21acce499d7597b64edd858f225e354d7350c1
Yrrej Dzun Deypalubos <yrrejdzun@gmail.com>
2024-11-16 08:17:12 +0800

    exclude emoji in comment, refresh button transaction

--- client/rate/script.js
+++ client/rate/script.js
@@ -222,3 +222,13 @@ function loadSpinner() {
 function hideSpinner() {
   document.getElementById("overlay").style.display = "none";
 }
+
+const noEmojiInput = document.getElementById("comment");
+
+noEmojiInput.addEventListener("input", function () {
+  // Filter out emojis using a regular expression
+  const regex = /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F1E6}-\u{1F1FF}]/gu;
+
+  // Remove any emoji characters from the input value
+  noEmojiInput.value = noEmojiInput.value.replace(regex, '');
+});
